959cd6c62e96768c2f74e85c4d827ecee9f0762a
Arunabh97 <dasarunabh036@gmail.com>
2024-03-28 14:07:08 +0530

    Fixed the code layout

--- food/checkout.php
+++ food/checkout.php
@@ -1,15 +1,13 @@
 <?php require "../config/config.php"; ?>
 <?php require "../libs/App.php"; ?>
 <?php require "../includes/header.php"; ?>
+
 <?php 
 
-
     if(!isset($_SERVER['HTTP_REFERER'])){
-        // redirect them to your desired location
         echo "<script>window.location.href='".APPURL."'</script>";
         exit;
     }
-
 
 ?>
 <?php 
